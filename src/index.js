039f3587ca9a8198ab5212d8ef323d72ccd92610
Nimra Naseem Awan <nimra66awan@gmail.com>
2024-07-03 07:20:17 -0500

    Created feedback modal

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const cancelBtn = document.querySelector(".cancel-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const scoreNumber = document.querySelectorAll(".number");
+
+function CloseModal(params) {
+  const close = document.querySelector(".container");
+  console.log(close);
+  close.style.display = "none";
+}
+function submitNumber(number) {
+  console.log(number, "number");
+
+  // Remove the active class from all buttons
+  var buttons = document.querySelectorAll(".number");
+  buttons.forEach(function (button) {
+    button.classList.remove("active");
+  });
+  console.log(buttons, "button");
+
+  // Add the active class to the clicked button
+  buttons[number - 1].classList.add("active");
+}
+cancelBtn.addEventListener("click", CloseModal);
